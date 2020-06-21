2859383ac32443d900913d5ca064c69bc4f4757c
Finn Evermann <finnevermann@gmx.de>
2020-06-21 18:02:39 +0200

    added build directory

---
+++ build/precache-manifest.bea5c2e7adf2b08fa1df454a58269053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ed610858d6abf40207b15ff2c5d0de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9334c78d0c6824b087c9",
+    "url": "/static/css/main.13dc82e4.chunk.css"
+  },
+  {
+    "revision": "02bd00629f9d305e2500",
+    "url": "/static/js/2.52ec4690.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.52ec4690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9334c78d0c6824b087c9",
+    "url": "/static/js/main.21b1e0c0.chunk.js"
+  },
+  {
+    "revision": "26c373e1bf1537f1187d",
+    "url": "/static/js/runtime-main.5d67aa28.js"
+  }
+]);
